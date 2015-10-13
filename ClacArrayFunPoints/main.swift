1c9527a7bde57121322e4b606b5198e290bfcbf0
Tony Fan <yf8@uw.edu>
2015-10-12 22:24:34 -0700

    Initial Commit

---
+++ ClacArrayFunPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ClacArrayFunPoints
+//
+//  Created by Yuyang Fan on 10/12/15.
+//  Copyright © 2015 Yuyang Fan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
