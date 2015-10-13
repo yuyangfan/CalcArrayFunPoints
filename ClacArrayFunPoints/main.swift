02cdad6476aa749ec612f57a2b7b975d0ee7f3be
Yuyang Fan <yf8@uw.edu>
2015-10-13 12:59:05 -0700

    assignment done

--- ClacArrayFunPoints/main.swift
+++ ClacArrayFunPoints/main.swift
@@ -52,11 +52,13 @@ func mathOp(left: Int?, right: Int?, op: (Int?, Int?) -> Int?) -> Int? {
         
 }
 
+print("Calculator:")
+print("3 + 2 is \(mathOp(3, right: 2, op: add)!)")
+print("3 - 2 is \(mathOp(3, right: 2, op: sub)!)")
+print("3 * 2 is \(mathOp(3, right: 2, op: multiply)!)")
+print("4 / 2 is \(mathOp(4, right: 2, op: divide)!)")
+print("")
 
-//print(mathOp(3, right: 2, op: add))
-//print(mathOp(3, right: 2, op: sub))
-//print(mathOp(3, right: 2, op: multiply))
-//print(mathOp(3, right: 2, op: divide))
 
 
 // Array Fun
@@ -113,14 +115,13 @@ func mathArrayOp (numbers: [Int]?, op: ([Int]? -> Int?)) -> Int? {
     return op(numbers)
 }
 
+print("Array Fun:")
+print("add array [1,2,3,4,5] is \(mathArrayOp([1,2,3,4,5], op: addArray)!)")
+print("multiply array [1,2,3,4,5] is \(mathArrayOp([1,2,3,4,5], op: multiArray)!)")
+print("avg array [1,2,3,4,5,6,7,8,9,10] is \(mathArrayOp([1,2,3,4,5,6,7,8,9,10], op: avg)!)")
+print("count array [1,2,3,4,5,6,7,8,9,10] is \(mathArrayOp([1,2,3,4,5,6,7,8,9,10], op: count)!)")
+print("")
 
-
-
-//print(addArray([1,2,3,4,5]))
-//print(multiArray([1,2,3,4,5]))
-//print(avg([1,2,3,4,5,6,7,8,9,10]))
-//print(count([1,2,3,4,5,6,7,8,9]))
-//print(mathArrayOp([1,2,3,4,5,6,7,8,9], op: avg))
 
 
 // Points: Tuples
@@ -158,12 +159,50 @@ func subPoints (point1: (Int?, Int?)?, point2: (Int?, Int?)?) -> (Int, Int)? {
     }
 }
 
-
-//print(subPoints((-2,-3), point2: (1,2)))
-//print(addPoints((1,3), point2: (1,nil)))
+print("Points:")
+print("(1,3) add (2,4) is \(addPoints((1,3), point2: (2,4))!)")
+print("(1,3) subtract (2,4) is \(subPoints((1,3), point2: (2,4))!)")
+print("")
 
 // Points: Dictionaries
+func addPoints (point1:[String:Double]?, point2: [String:Double]?) -> (Double, Double)? {
+    if(point1 != nil && point2 != nil){
+        let x1 = point1!["x"]
+        let y1 = point1!["y"]
+        let x2 = point2!["x"]
+        let y2 = point2!["y"]
+        
+        if(x1 != nil && x2 != nil && y1 != nil && y2 != nil) {
+            
+            return (x1! + x2!, y1! + y2!)
+        }
+    }
+        return nil
+}
 
-//func addPointsDict (point1: Dictionary<String, Int>?, point2: Dictionary<String, Int>?) -> (Int, Int)? {
-//    
-//}
+func subPoints (point1:[String:Double]?, point2: [String:Double]?) -> (Double, Double)? {
+    if(point1 != nil && point2 != nil){
+        let x1 = point1!["x"]
+        let y1 = point1!["y"]
+        let x2 = point2!["x"]
+        let y2 = point2!["y"]
+        
+        if(x1 != nil && x2 != nil && y1 != nil && y2 != nil) {
+            
+            return (x1! - x2!, y1! - y2!)
+        }
+    }
+    return nil
+}
+
+
+print("[\"x\":2.3, \"y\":3.2] adds [\"x\":3.3, \"y\":4.5] is ")
+print(addPoints(["x":2.3, "y":3.2], point2: ["x":3.3, "y":4.5])!)
+
+print("[\"x\":10.8, \"y\":10.2] subtracts [\"x\":17.1, \"y\":4.1] is ")
+print(subPoints(["x":10.8, "y":10.2], point2: ["x":17.1, "y":4.1])!)
+
+
+
+
+
